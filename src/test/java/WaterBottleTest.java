c7d28a03f07cbeb6f10c5a8333704d7bebed304e
Greg Shanks <greg.shanks@gmail.com>
2020-01-27 16:38:09 +0000

    Calculator tests and methods written. Tests ran successfully.

--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+
+        private WaterBottle waterBottle;
+
+        @Before
+        public void before(){
+            waterBottle = new WaterBottle(100);
+        }
+
+        @Test
+        public void canDrink() {
+            waterBottle.drink();
+            assertEquals(90, waterBottle.getVolume());
+        }
+
 }
