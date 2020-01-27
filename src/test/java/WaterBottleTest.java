9c637b80c5672bee88e4d339a65cc893d36d85ef
Greg Shanks <greg.shanks@gmail.com>
2020-01-27 16:44:27 +0000

    WaterBottle Class methods and tests written. Tests ran successfully.

--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -18,4 +18,17 @@ public class WaterBottleTest {
             assertEquals(90, waterBottle.getVolume());
         }
 
+        @Test
+        public void canEmpty() {
+            waterBottle.empty();
+            assertEquals(0, waterBottle.getVolume());
+        }
+
+        @Test
+         public void canFill() {
+            waterBottle.empty();
+            waterBottle.fill();
+            assertEquals(100, waterBottle.getVolume());
+        }
+
 }
