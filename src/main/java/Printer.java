e43ec81fec4c6b853cb8c0b49364ba62bfc87cee
Greg Shanks <greg.shanks@gmail.com>
2020-01-27 16:52:08 +0000

    Created Printer Class with paper and toner properties. Wrote tests to check object initialises correctly with values of 100 for paper and toner. Tests ran successfully.

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,5 +1,19 @@
 public class Printer {
 
+    private int paper;
+    private int toner;
+
+    public Printer(int paper, int toner){
+        this.paper = paper;
+        this.toner = toner;
+    }
 
 
+    public Object getPaper() {
+        return this.paper;
+    }
+
+    public Object getToner() {
+        return this.toner;
+    }
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,5 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PrinterTest {
 
+    private Printer printer;
 
+    @Before
+    public void before(){
+        printer = new Printer(100, 100);
+    }
+
+    @Test
+    public void hasPaper(){
+        assertEquals(100, printer.getPaper());
+    }
+
+    @Test
+    public void hasToner(){
+        assertEquals(100, printer.getToner());
+    }
 
 }
